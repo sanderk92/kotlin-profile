e28c22e56df001c0f84fe6d337d178e19b8cf2ed
Sander <sanderkrabbenborg@hotmail.com>
2023-05-06 17:31:32 +0200

    Also implement ChronoField QuarterOfDay

--- src/main/kotlin/Ptu.kt
+++ src/main/kotlin/Ptu.kt
@@ -1,7 +1,5 @@
-import java.time.Duration
-import java.time.LocalDate
-import java.time.ZoneId
-import java.time.ZonedDateTime
+import java.time.*
+import java.time.LocalTime.MIDNIGHT
 import java.time.temporal.*
 import java.time.temporal.ChronoField.MINUTE_OF_DAY
 import java.time.temporal.ChronoUnit.*
@@ -20,9 +18,17 @@ class Ptu private constructor(val date: LocalDate, val index: Int, val zone: Zon
      */
     constructor(zonedDateTime: ZonedDateTime) : this(
         date = zonedDateTime.toLocalDate(),
-        index = (zonedDateTime.truncatedTo(DAYS).until(zonedDateTime, MINUTES) / Quarters.duration.toMinutes()).toInt(),
+        index = QuarterOfDay.getFrom(zonedDateTime).toInt(),
         zone = zonedDateTime.zone,
     )
+
+    override fun isSupported(unit: TemporalUnit): Boolean {
+        return true
+    }
+
+    override fun isSupported(field: TemporalField): Boolean {
+        return true
+    }
 
     override fun getLong(field: TemporalField): Long {
         return zonedDateTime.getLong(field)
@@ -40,24 +46,37 @@ class Ptu private constructor(val date: LocalDate, val index: Int, val zone: Zon
         return Ptu(zonedDateTime.with(field, newValue))
     }
 
+    override fun hashCode(): Int {
+        return zonedDateTime.hashCode()
+    }
+
+    override fun toString(): String {
+        return "Ptu(index=$index,time=$zonedDateTime)"
+    }
+
+    override fun compareTo(other: Ptu): Int {
+        return zonedDateTime.until(other, NANOS).sign
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
         return zonedDateTime == (other as Ptu).zonedDateTime
     }
-
-    override fun isSupported(unit: TemporalUnit): Boolean = true
-    override fun isSupported(field: TemporalField): Boolean = true
-    override fun hashCode(): Int = zonedDateTime.hashCode()
-    override fun toString(): String = "Ptu(index=$index,time=$zonedDateTime)"
-    override fun compareTo(other: Ptu): Int = zonedDateTime.until(other, NANOS).sign
 }
 
-@Suppress("UNCHECKED_CAST")
 object Quarters : TemporalUnit {
-    override fun isTimeBased(): Boolean = true
-    override fun isDateBased(): Boolean = false
-    override fun isDurationEstimated(): Boolean = false
+    override fun isTimeBased(): Boolean {
+        return true
+    }
+
+    override fun isDateBased(): Boolean {
+        return false
+    }
+
+    override fun isDurationEstimated(): Boolean {
+        return false
+    }
 
     override fun getDuration(): Duration {
         return Duration.of(15, MINUTES)
@@ -67,7 +86,49 @@ object Quarters : TemporalUnit {
         return temporal.until(other, MINUTES) / duration.toMinutes()
     }
 
+    @Suppress("UNCHECKED_CAST")
     override fun <R : Temporal> addTo(temporal: R, amount: Long): R {
         return temporal.plus(duration.multipliedBy(amount)) as R
+    }
+}
+
+object QuarterOfDay : TemporalField {
+    override fun isDateBased(): Boolean {
+        return false
+    }
+
+    override fun isTimeBased(): Boolean {
+        return true
+    }
+
+    override fun getBaseUnit(): TemporalUnit {
+        return MINUTES
+    }
+
+    override fun getRangeUnit(): TemporalUnit {
+        return MINUTES
+    }
+
+    override fun range(): ValueRange {
+        return ValueRange.of(0, 99)
+    }
+
+    override fun rangeRefinedBy(temporal: TemporalAccessor): ValueRange {
+        return ValueRange.of(0, 99) // TODO could be further refined
+    }
+
+    override fun isSupportedBy(temporal: TemporalAccessor): Boolean {
+        return temporal is Instant || temporal is ZonedDateTime
+    }
+
+    override fun getFrom(temporal: TemporalAccessor): Long = when (temporal) {
+        is Instant -> temporal.truncatedTo(DAYS).until(temporal, MINUTES) / Quarters.duration.toMinutes()
+        is ZonedDateTime -> temporal.truncatedTo(DAYS).until(temporal, MINUTES) / Quarters.duration.toMinutes()
+        else -> throw UnsupportedTemporalTypeException("$temporal is not supported")
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <R : Temporal> adjustInto(temporal: R, newValue: Long): R {
+        return temporal.with(Quarters.addTo(MIDNIGHT, newValue)) as R
     }
 }
